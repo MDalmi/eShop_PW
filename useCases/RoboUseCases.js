3e78404404fe349fdc605a6274792e42480aad0f
MDalmi <maicondalmina.pf003@academico.ifsul.edu.br>
2024-11-11 10:14:55 -0300

    .

--- useCases/RoboUseCases.js
+++ useCases/RoboUseCases.js
@@ -60,7 +60,11 @@ const deleteRoboDB = async (codigo) => {
 
 const getRoboPorCodigoDB = async (codigo) => {
     try {
-        const results = await pool.query(`SELECT * FROM robos WHERE codigo = $1`,
+        const results = await pool.query(`SELECT R.codigo AS codigo, R.nome AS nome,          
+        R.capacidade_max AS capacidade, R.descricao as descricao, R.valor_aluguel AS valor_aluguel, R.tipo AS tipo
+        FROM robos R WHERE codigo = $1 
+        RETURNING codigo, nome, 
+        capacidade_max, descricao, valor_aluguel, tipo  `,
             [codigo]);
         if (results.rowCount == 0) {
             throw "Nenhum registro encontrado com o código: " + codigo;
