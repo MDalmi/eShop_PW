c452f03e121d37c1a917995223d4c4b1f97e3dfe
MDalmi <maicondalmina.pf003@academico.ifsul.edu.br>
2024-12-09 08:47:51 -0300

    .

--- useCases/AluguelUseCase.js
+++ useCases/AluguelUseCase.js
@@ -22,7 +22,7 @@ const addAluguelDB = async (body) => {
         const { nome, robo, planeta, descricao } = body;
         const results = await pool.query(`INSERT INTO aluguel_robos (nome, robo , planeta, descricao)
             VALUES ($1, $2, $3, $4) 
-            RETURNING  nome, robo, planeta, descricao_mis`,
+            RETURNING  nome, robo, planeta, descricao`,
             [nome, robo, planeta, descricao]);
         const aluguel = results.rows[0];
         return new Aluguel(aluguel.codigo, aluguel.nome,
@@ -39,7 +39,7 @@ const updateAluguelDB = async (body) => {
         const results = await pool.query(`UPDATE aluguel_robos SET nome = $2, 
              robo = $3, planeta = $4, descricao = $5
              WHERE codigo = $1
-             RETURNING codigo, nome, robo, planeta, descricao_mis`,
+             RETURNING codigo, nome, robo, planeta, descricao`,
             [codigo, nome, robo, planeta, descricao]);
         if (results.rowCount == 0) {
             throw `Nenhum registro encontrado com o código ${codigo}
