d54dce9707ee3b0194b9925e775efa18f865bd2b
MDalmi <maicondalmina.pf003@academico.ifsul.edu.br>
2024-12-08 22:51:06 -0300

    .

--- config.js
+++ config.js
@@ -5,20 +5,27 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 let pool = null;
 
-if (isProduction){
+if (isProduction) {
     pool = new Pool({
-        connectionString : process.env.DATABASE_URL,
-        ssl : {
-            rejectUnauthorized : false
+        min: 0,
+        max: 10,
+        createTimeoutMillis: 8000,
+        acquireTimeoutMillis: 8000,
+        idleTimeoutMillis: 8000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 100,
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
         }
     })
 } else {
     pool = new Pool({
-        user : 'postgres',
-        password : 'postgres',
-        database : 'testePW',
-        host : 'localhost',
-        port : 5432
+        user: 'postgres',
+        password: 'postgres',
+        database: 'testePW',
+        host: 'localhost',
+        port: 5432
     }
     )
 }
