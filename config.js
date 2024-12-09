e1f7bc7c3b7e5fac2a77c11a6db92097efd18550
MDalmi <maicondalmina.pf003@academico.ifsul.edu.br>
2024-12-08 23:00:24 -0300

    .

--- config.js
+++ config.js
@@ -7,13 +7,6 @@ let pool = null;
 
 if (isProduction) {
     pool = new Pool({
-        min: 0,
-        max: 10,
-        createTimeoutMillis: 8000,
-        acquireTimeoutMillis: 8000,
-        idleTimeoutMillis: 8000,
-        reapIntervalMillis: 1000,
-        createRetryIntervalMillis: 100,
         connectionString: process.env.DATABASE_URL,
         ssl: {
             rejectUnauthorized: false
